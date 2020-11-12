1814421106828234041046f0436daec7e3005635
Manjunath P <manjup2128@gmail.com>
2020-11-12 15:14:10 +0530

    Adding input form

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,url_for,redirect
 #from flask_bootstrap import Bootstrap
 from My_Sql_data import BLOOD
 
 app=Flask(__name__)
 #Bootstrap(app)
+
+helper=BLOOD()
 
 @app.route('/')
 def index():
@@ -13,9 +15,19 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/donate')
+@app.route('/donate',methods=["POST","GET"])
 def donate():
-    return render_template('donate.html')
+    if request.method=="POST":
+        name=request.form['name']
+        date=request.form['Date']
+        gender=request.form['gender']
+        group=request.form['groups']
+        location=request.form['location']
+        quantity=request.form['quant']
+        age=request.form['age']
+        return redirect(url_for('thanks'))
+    else:
+        return render_template('donate.html')
 
 @app.route('/checking')
 def checking():
@@ -24,6 +36,11 @@ def checking():
 @app.route('/bloodbank')
 def bloodbank():
     return render_template('bloodbank.html')
+
+@app.route('/thanks')
+def thanks():
+    return render_template('thankyou.html')
+
 
 
 if __name__=='__main__':
