7ca5b1f0f2cc1d0399ebad26c33aff11edeb49c8
Manjunath P <manjup2128@gmail.com>
2021-01-15 09:41:39 +0530

    Complete code testing is remaining

--- My_Sql_data.py
+++ My_Sql_data.py
@@ -1,15 +1,29 @@
 import mysql.connector as connector
+import random
 class BLOOD:
     def __init__(self):
         self.con=connector.connect(host='localhost',port=3306,user='root',password='Manju@123',database='blood_donation')
         query='create table if not exists bloodbank (id int AUTO_INCREMENT PRIMARY KEY,donar_name varchar(20),date varchar(10),gender varchar(10),age int,blood_group varchar(20),location varchar(50),quantity float)'
+        query1='create table if not exists blood_loc(blood_id int,blood_group varchar(20),quant float,locate varchar(50),foreign key (blood_id) references bloodbank(id))'
+        query2='create table if not exists accept_info(id int AUTO_INCREMENT PRIMARY KEY,acceptor_name varchar(20),location varchar(50),blood_group varchar(20),phone_no varchar(20),amount_blood int)'
         cur=self.con.cursor()
         cur.execute(query)
+        cur.execute(query1)
+        cur.execute(query2)
         print('Created')
 
     def insert_query(self,donor_name,date,gender,age,blood_group,location,quantity):
         query="insert into bloodbank(donar_name,date,gender,age,blood_group,location,quantity) values ('{}','{}','{}',{},'{}','{}',{})".format(donor_name,date,gender,age,blood_group,location,quantity)
         cur=self.con.cursor()
+        cur.execute(query)
+        self.con.commit()
+        query="select id from bloodbank where donar_name='{}'".format(donor_name)
+        cur=self.con.cursor()
+        cur.execute(query)
+        li=[]
+        for id_val in cur:
+            li.append(id_val)
+        query="insert into blood_loc values ({},'{}',{},'{}')".format(int(li[0][0]),blood_group,quantity,location)
         cur.execute(query)
         self.con.commit()
         print("Inserted")
@@ -22,6 +36,48 @@ class BLOOD:
         for row in cur:
             li.append(row)
         return li
+
+    def get_by_location_group(self,group,loc):
+        query="select blood_group,sum(quant) from blood_loc where blood_group='{}' and  locate='{}'".format(group,loc)
+        cur=self.con.cursor()
+        cur.execute(query)
+        li=[]
+        for row in cur:
+            li.append(row)
+        return li
+    def add_accept_info(self,name,loc,blood_group,phone_num,amount):
+        query="insert into accept_info(acceptor_name,location,blood_group,phone_no,amount_blood) values ('{}','{}','{}','{}',{})".format(name,loc,blood_group,phone_num,amount)
+        cur=self.con.cursor()
+        cur.execute(query)
+        self.con.commit()
+    def delete_from_bank(self,blood_group,location,quantity):
+        query="select blood_id from blood_loc where blood_group='{}' and locate='{}'".format(blood_group,location)
+        cur=self.con.cursor()
+        cur.execute(query)
+        li=[]
+        for row in cur:
+            li.append(row)
+        print(li)
+        li1=[li[i][0] for i in range(len(li))]
+        print(li1)
+        de=random.sample(li1,int(quantity))
+        print(de)
+        for value in de:
+            query="delete from blood_loc where blood_id={}".format(value)
+            cur=self.con.cursor()
+            cur.execute(query)
+            self.con.commit()
+    
+    def get_by_group(self,group):
+        query="select blood_group,sum(quant),locate from blood_loc where blood_group='{}' group by locate;".format(group)
+        cur=self.con.cursor()
+        cur.execute(query)
+        li=[]
+        for row in cur:
+            li.append(row)
+        return li
+           
+        
 
     def get_info(self):
         query='select * from bloodbank'
@@ -41,3 +97,5 @@ class BLOOD:
             li.append(row)
         return li
         
+# h=BLOOD()
+# h.insert_query('ABC','blal','male',12,'blall','ooo',1)
--- main.py
+++ main.py
@@ -42,16 +42,64 @@ def checking():
 def bloodbank():
     if request.method=="POST":
         loc=request.form['location1']
+        li=helper.get_by_location(loc)
         print(loc)
-        return "<h1>Success</h1>"
-        #blood_list=helper.get_info()
+        print(li)
+        if len(li)==0:
+            return redirect(url_for('sorry',loc=loc))
+        else:
+            return render_template('show_info.html',li=li[::-1])
+        #return "<h1>Success</h1>"
     else:
         return render_template('bloodbank.html')
+
+@app.route('/sorry/<loc>')
+def sorry(loc):
+    return render_template('sorry.html',loc=loc)
 
 @app.route('/<name>')
 def thanks(name):
     return render_template('thankyou.html',name=name)
 
+@app.route('/bloodneed',methods=['POST','GET'])
+def bloodneed():
+    if request.method=="POST":
+        group=request.form['groups1']
+        loc=request.form['location2']
+        li=helper.get_by_location_group(group,loc)
+        print(li)
+        if li[0][0]==None or li[0][1]==None:
+            return render_template('so_sad.html',group=group,loc=loc)
+        else:
+            print(type(li))
+            return redirect(url_for('gotblood',li=li)) 
+    else:
+        return render_template('bloodneed.html')
+
+@app.route('/so_sad',methods=['POST','GET'])
+def others():
+    if request.method=='POST':
+        group=request.form['groups1']
+        data=helper.get_by_group(group)
+        print(data)
+        return render_template("otheroptions.html",data=data)
+    else:
+        return render_template('searchblood.html')
+
+@app.route('/gotblood/<li>',methods=["POST","GET"])
+def gotblood(li):
+    if request.method=="POST":
+        name=request.form['name']
+        loc=request.form['location']
+        number=request.form['phone']
+        quant=request.form['quantity']
+        group=request.form['groups']
+        helper.add_accept_info(name,loc,group,number,int(quant))
+        helper.delete_from_bank(group,loc,quant)
+        return render_template('afteraccept.html',name=name)
+    else:
+        num=int(li[16:17])
+        return render_template('gotblood.html',li=li,num=num)
 
 
 if __name__=='__main__':
